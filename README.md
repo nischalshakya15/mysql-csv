942554fe312fa205ee105fa0ca16a4af25472fc9
nischalshakya95 <nischalshakya@lftechnology.com>
2020-05-02 21:13:12 +0545

    Finished importing csv file into database table
    remove csv file

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-### References
-* https://jakevdp.github.io/PythonDataScienceHandbook/02.01-understanding-data-types.html
-* https://www.geeksforgeeks.org/python-dictionary/
-* https://www.geeksforgeeks.org/how-to-get-column-names-in-pandas-dataframe/
-* https://www.mysqltutorial.org/mysql-export-table-to-csv/
-* https://pythontic.com/pandas/serialization/mysql
+## mysql-csv 
+A simple command line utility which helps to export the csv file directly into database without a hassle to create a table 
+and specify a data type. 
+
+## Purpose
+It is created solely for the purpose to help data analyst for the data manipulation and getting the result using an sql query 
+which in turns help analyst to get the required information before hand. This information will help analyst to expect what type of information 
+should a data frame hold for data visualization.
+
+## Prerequisites 
+* python 
+* pip 
+* pandas
+* numpy 
+* SQLAlchemy
+* setup-tools
+
+## Help
+View the usage of command line parameters.
+
+``python mysql-csv.py --help`` 
+
+```
+usage: mysql-csv.py [-h] [--hostname] --file [--database] [--database-type] [--username] [--password] [--table] [--dialect]
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --hostname       domain name or ip address of database
+  --file           csv file to be exported to table
+  --database       database name
+  --database-type  database type mysql | sql | oracle | postgres
+  --username       username of database
+  --password       password of database
+  --table          table where csv to be imported
+  --dialect        database dialect pymysql for mysql
+```
+
+## Import csv file into database
+```
+python mysql-csv.py --hostname localhost --file hotel_bookings.csv --database demo --database-type mysql --username root --password root --table=something --dialect pymysql
+```
--- mysql-csv.py
+++ mysql-csv.py
@@ -1,23 +1,40 @@
+import argparse
+
 import pandas as pd
 from sqlalchemy import create_engine
 
-CSV_FILE = './hotel_bookings.csv'
-DATABASE_TYPE = 'mysql'
-USERNAME = 'root'
-PASSWORD = 'root'
-DATABASE = 'demo'
-TABLE = 'demo_hotel'
-HOST = 'localhost'
+parser = argparse.ArgumentParser()
+parser.add_argument("--hostname", dest="hostname", metavar="\b", default="localhost",
+                    help='domain name or ip address of database')
+parser.add_argument("--file", type=str, dest="csv_file", metavar="\b", required=True,
+                    help="csv file to be exported to table")
+parser.add_argument("--database", type=str, dest="database", metavar="\b", required=True, help="database name")
+parser.add_argument("--database-type", type=str, dest="database_type", metavar="\b", required=True,
+                    help="database type mysql | sql | oracle | postgres")
+parser.add_argument("--username", type=str, dest="username", metavar="\b", required=True, help="username of database")
+parser.add_argument("--password", type=str, dest="password", metavar="\b", required=True, help="password of database")
+parser.add_argument("--table", type=str, dest="table", metavar="\b", required=True,
+                    help="table where csv to be imported")
+parser.add_argument("--dialect", type=str, dest="dialect", metavar="\b", required=True,
+                    help="database dialect pymysql for mysql")
+args = parser.parse_args()
 
-DB_URL = DATABASE_TYPE + "+pymysql://" + USERNAME + ":" + PASSWORD + "@" + HOST + "/" + DATABASE
+CSV_FILE = args.csv_file
+DATABASE_TYPE = args.database_type
+USERNAME = args.username
+PASSWORD = args.password
+DATABASE = args.database
+TABLE = args.table
+HOST = args.hostname
+DIALECT = args.dialect
 
-df = pd.read_csv(CSV_FILE)
-engine = create_engine(DB_URL, pool_recycle=3600)
+DB_URL = DATABASE_TYPE + "+" + DIALECT + "://" + USERNAME + ":" + PASSWORD + "@" + HOST + "/" + DATABASE
 
-with engine.connect() as conn, conn.begin():
-    try:
+try:
+    df = pd.read_csv(CSV_FILE)
+    engine = create_engine(DB_URL, pool_recycle=3600)
+    with engine.connect() as conn, conn.begin():
         df.to_sql(TABLE, conn, if_exists='replace', index=False)
-    except Exception as err:
-        print(err.__cause__)
-    finally:
-        print("CSV successfully imported")
+    print("CSV successfully imported")
+except Exception as err:
+    print(err.__cause__)
